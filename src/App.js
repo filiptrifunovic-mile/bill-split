d68c4731a158b42dcbaa0cd19d24f78ff51c17bf
Filip Trifunovic <114927397+filiptrifunovic-mile@users.noreply.github.com>
2023-11-10 16:48:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -69,6 +69,7 @@ export default function App() {
         <FormSplitBill
           handleSplitBill={handleSplitBill}
           selectedFriend={selectedFriend}
+          key={selectedFriend.id}
         />
       )}
     </div>
